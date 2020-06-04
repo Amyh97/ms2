7068b56c6a138073eebd52377861b9f605dab00e
Amyh97 <amy.hollis@btinternet.com>
2020-06-04 18:14:17 +0000

    add input fields for accomodation.html and created hotels.js

--- assets/js/flights.js
+++ assets/js/flights.js
@@ -10,12 +10,14 @@ xhr.withCredentials = true;
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
         console.log(this.responseText);
-	}
+        document.getElementById("results").innerHTML(`
+        <p>The cheapest flight found was £${Quotes[0].MinPrice}, flying with ${help}</p>`);
+	} 
 });
 
-xhr.open("GET", `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en/LHR-sky/BUD-sky/${dateOut}?inboundpartialdate=${dateIn}`);
+xhr.open("GET", `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en/${dep}-sky/PRG-sky/${dateOut}?inboundpartialdate=${dateIn}`);
 xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa");
 
 xhr.send(data);
-})
+});
