0a5d79dc238a2632493dc7413822032b1f729a18
Amyh97 <amy.hollis@btinternet.com>
2020-06-06 19:19:27 +0000

    remove js error messages and correct id name in sights.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,17 @@ async function makeAPICall(url, headers) {
   return response.json(); 	
 }
 
-$("#convert").click(function() {
+$("#convert").click(function () {
     	var amount = $("#amount").val();
-	const result = makeAPICall(`https://currency-exchange.p.rapidapi.com/exchange?q=${amount}&from=GBP&to=CZK`, {
+	makeAPICall(`https://currency-exchange.p.rapidapi.com/exchange?q=${amount}&from=GBP&to=CZK`, {
 		"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
-		"x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa"	
-	});
-
+		"x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa"
+    });
 });
 
-$("searchHotel").click(function(){
-    const result = makeAPICall("https://tripadvisor1.p.rapidapi.com/hotels/list?offset=0&currency=GBP&limit=30&order=asc&lang=en_US&sort=recommended&location_id=274707&adults=2&checkin=%3Crequired%3E&rooms=1&nights=2", {
+
+$("#searchHotel").click(function(){
+    makeAPICall("https://tripadvisor1.p.rapidapi.com/hotels/list?offset=0&currency=GBP&limit=30&order=asc&lang=en_US&sort=recommended&location_id=274707&adults=2&checkin=%3Crequired%3E&rooms=1&nights=2", {
         "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
         "x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa"
     });
@@ -26,7 +26,7 @@ $("#search").click(function() {
     var dateIn = $("#dateIn").val();
     var dep = $("#dep").val();
 
-    const result = makeAPICall(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en/${dep}-sky/PRG-sky/${dateOut}?inboundpartialdate=${dateIn}`, {
+    makeAPICall(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en/${dep}-sky/PRG-sky/${dateOut}?inboundpartialdate=${dateIn}`, {
         "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
         "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
     });
