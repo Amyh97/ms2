4bb797f25d34bc8533507b7582036ebe40dae231
Amyh97 <amy.hollis@btinternet.com>
2020-06-08 16:19:51 +0000

    implement new header ascross pages and correct syntax on maps.js to show markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,33 +1,33 @@
-var castle = {lat:50.0911, lan:14.4016};
-var stVitus ={lat:50.0909, lng:14.4005};
-var charlesBridge = {lat:50.0865, lng:14.4114};
-var clock = {lat:50.0870, lng:14.4207};
-var oldTown = {lat:50.0876, lng:14.4212};
-var wenceslasSquare = {lat:50.0810, lng:14.4280};
-var powderTower = {lat:50.0873, lng:14.42780};
-var lennonWall = {lat:50.0863, lng:14.4068};
-var dancingHouse = {lat:50.0754, lng:14.4142};
-var tyn = {lat:50.0877, lng:14.4227}
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: {
             lat: 50.0755,
-            lng: 14.4378
-        }
+            lng: 14.4378,
+        },
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var location = [castle, stVitus,charlesBridge,clock,oldTown,wenceslasSquare,powderTower,lennonWall,dancingHouse,tyn];
+    var location = [
+        { lat: 50.0911, lan: 14.4016 }, //castle
+        { lat: 50.0909, lng: 14.4005 }, //StVitus
+        { lat: 50.0865, lng: 14.4114 }, //CahrlesBridge
+        { lat: 50.087, lng: 14.4207 }, //clock
+        { lat: 50.0876, lng: 14.4212 }, //oldTownSquare
+        { lat: 50.081, lng: 14.428 }, //wencelasSquare
+        { lat: 50.0873, lng: 14.4278 }, //powdewrtower
+        { lat: 50.0863, lng: 14.4068 }, //lennonWall
+        { lat: 50.0754, lng: 14.4142 }, //Dancing House
+        { lat: 50.0877, lng: 14.4227 }, //Tyn
+    ];
 
-    var markers = location.map(function(location, i) {
+    var markers = location.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
-            label: labels[i % labels.length]
+            label: labels[i % labels.length],
         });
     });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
 }
