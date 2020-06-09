e33b124b2539499b427a93a4e10b5d784d6507cf
Amyh97 <amy.hollis@btinternet.com>
2020-06-09 14:51:17 +0000

    fix, define location before reading it

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,18 +9,7 @@ function initMap() {
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
-    var markers = location.map(function (location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length],
-        });
-    });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
-}
-
- var location = [
+    var location = [
         { lat: 50.0911, lan: 14.4016 }, 
         { lat: 50.0909, lng: 14.4005 }, 
         { lat: 50.0865, lng: 14.4114 }, 
@@ -32,3 +21,16 @@ function initMap() {
         { lat: 50.0754, lng: 14.4142 }, 
         { lat: 50.0877, lng: 14.4227 }, 
     ];
+
+
+    var markers = location.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length],
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
+}
+
+ 
