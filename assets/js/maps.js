15f6b3a1ad46a171c12cb8707fa56965a0685fb6
Amyh97 <amy.hollis@btinternet.com>
2020-06-09 14:49:04 +0000

    move locations to bottom of script as per documentation and adjust zoom

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 11,
         center: {
             lat: 50.0755,
             lng: 14.4378,
@@ -9,18 +9,6 @@ function initMap() {
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var location = [
-        { lat: 50.0911, lan: 14.4016 }, //castle
-        { lat: 50.0909, lng: 14.4005 }, //StVitus
-        { lat: 50.0865, lng: 14.4114 }, //CahrlesBridge
-        { lat: 50.087, lng: 14.4207 }, //clock
-        { lat: 50.0876, lng: 14.4212 }, //oldTownSquare
-        { lat: 50.081, lng: 14.428 }, //wencelasSquare
-        { lat: 50.0873, lng: 14.4278 }, //powdewrtower
-        { lat: 50.0863, lng: 14.4068 }, //lennonWall
-        { lat: 50.0754, lng: 14.4142 }, //Dancing House
-        { lat: 50.0877, lng: 14.4227 }, //Tyn
-    ];
 
     var markers = location.map(function (location, i) {
         return new google.maps.Marker({
@@ -31,3 +19,16 @@ function initMap() {
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
 }
+
+ var location = [
+        { lat: 50.0911, lan: 14.4016 }, 
+        { lat: 50.0909, lng: 14.4005 }, 
+        { lat: 50.0865, lng: 14.4114 }, 
+        { lat: 50.087, lng: 14.4207 }, 
+        { lat: 50.0876, lng: 14.4212 }, 
+        { lat: 50.081, lng: 14.428 }, 
+        { lat: 50.0873, lng: 14.4278 }, 
+        { lat: 50.0863, lng: 14.4068 }, 
+        { lat: 50.0754, lng: 14.4142 }, 
+        { lat: 50.0877, lng: 14.4227 }, 
+    ];
