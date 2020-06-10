29860188c47e30dd894fd2aaccc2228d49b5011a
Amyh97 <amy.hollis@btinternet.com>
2020-06-10 09:29:19 +0000

    correct coordinates form markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,36 +1,30 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 11,
-        center: {
-            lat: 50.0755,
-            lng: 14.4378,
-        },
-    });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    var location = [
-        { lat: 50.0911, lan: 14.4016 }, 
-        { lat: 50.0909, lng: 14.4005 }, 
-        { lat: 50.0865, lng: 14.4114 }, 
-        { lat: 50.087, lng: 14.4207 }, 
-        { lat: 50.0876, lng: 14.4212 }, 
-        { lat: 50.081, lng: 14.428 }, 
-        { lat: 50.0873, lng: 14.4278 }, 
-        { lat: 50.0863, lng: 14.4068 }, 
-        { lat: 50.0754, lng: 14.4142 }, 
-        { lat: 50.0877, lng: 14.4227 }, 
-    ];
-
-
-    var markers = location.map(function (location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length],
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: {lat: 50.0755, lng: 14.4378}
         });
-    });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
-}
+                var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
- 
+                var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = [
+        {lat:50.0911, lng:14.4016},
+        {lat:50.0909, lng:14.4005},
+        {lat:50.0865, lng:14.4114},
+        {lat:50.0870, lng:14.4207},
+        {lat:50.0810, lng:14.4280},
+        {lat:50.0873, lng:14.4278},
+        {lat:50.0863, lng:14.4068},
+        {lat:50.0754, lng:14.4142},
+        {lat:50.0877, lng:14.4227}
+      ]
