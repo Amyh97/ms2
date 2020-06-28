5ad2926dc58a4fb0c4c46b69f73315f20ff403e9
Amyh97 <amy.hollis@btinternet.com>
2020-06-28 19:58:24 +0000

    add comments, fix to pass validator and format code

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,30 +1,29 @@
 function initMap() {
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: {lat: 50.0755, lng: 14.4378}
-        });
-
-                var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-                var markers = locations.map(function(location, i) {
-          return new google.maps.Marker({
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: { lat: 50.0755, lng: 14.4378 },
+    });
+    //array of lables to be used by markers
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //use lables on markers for each location from location array
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
             position: location,
-            label: labels[i % labels.length]
-          });
+            label: labels[i % labels.length],
         });
+    });
 
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      }
-      var locations = [
-        {lat:50.0911, lng:14.4016},
-        {lat:50.0909, lng:14.4005},
-        {lat:50.0865, lng:14.4114},
-        {lat:50.0870, lng:14.4207},
-        {lat:50.0810, lng:14.4280},
-        {lat:50.0873, lng:14.4278},
-        {lat:50.0863, lng:14.4068},
-        {lat:50.0754, lng:14.4142},
-        {lat:50.0877, lng:14.4227}
-      ]
+    //marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
+}
+var locations = [
+    { lat: 50.0911, lng: 14.4016 },
+    { lat: 50.0909, lng: 14.4005 },
+    { lat: 50.0865, lng: 14.4114 },
+    { lat: 50.087, lng: 14.4207 },
+    { lat: 50.081, lng: 14.428 },
+    { lat: 50.0873, lng: 14.4278 },
+    { lat: 50.0863, lng: 14.4068 },
+    { lat: 50.0754, lng: 14.4142 },
+    { lat: 50.0877, lng: 14.4227 },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,76 @@
+//general function to call API
 async function makeAPICall(url, headers) {
-  const response = await fetch(url, {
-    headers
-  });
-  return response.json();
-}
-
-$("#convert").on("click",function (event) {
-    event.preventDefault();
-        var amount = $("#amount").val();
-            data = (makeAPICall(
-	    `https://currency-converter13.p.rapidapi.com/convert?amount=${amount}&from=GBP&to=CZK`, {
-		"x-rapidapi-host": "currency-converter13.p.rapidapi.com",
-        "x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa"}));
-        data.then((value) => {
-        var converstion = value.amount.toFixed(2);
-        document.getElementById("convetred").append("There are "+converstion+"kč to £"+amount);
-        
-        });
-
+    const response = await fetch(url, {
+        headers,
     });
-
-
-
-$("#searchHotel").on("click",function(event){
+    return response.json();
+}
+//converter API
+$("#convert").on("click", function (event) {
+    //prevent default to stop refresh
     event.preventDefault();
+    //amount to populate URL for API call
+    var amount = $("#amount").val();
+    data = makeAPICall(`https://currency-converter13.p.rapidapi.com/convert?amount=${amount}&from=GBP&to=CZK`, {
+        "x-rapidapi-host": "currency-converter13.p.rapidapi.com",
+        "x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa",
+    });
+    data.then((value) => {
+        //toFixed to have 2 decimal places
+        var converstion = value.amount.toFixed(2);
+        //use API data to provide results
+        document.getElementById("convetred").append("There are " + converstion + "kč to £" + amount);
+    });
+});
+
+//Hotel API
+$("#searchHotel").on("click", function (event) {
+    //preventDefault to stop refresh
+    event.preventDefault();
+    //credentials to populate URL for API
     var ci = $("#ci").val();
     var adults = $("#people").val();
     var rooms = $("#rooms").val();
     var nights = $("#nights").val();
-    data = (makeAPICall(`https://tripadvisor1.p.rapidapi.com/hotels/list?offset=0&currency=GBP&limit=30&order=asc&lang=en_US&sort=recommended&location_id=274707&adults=${adults}1&checkin=${ci}&rooms${rooms}1&nights=${nights}`, {
+    data = makeAPICall(`https://tripadvisor1.p.rapidapi.com/hotels/list?offset=0&currency=GBP&limit=30&order=asc&lang=en_US&sort=recommended&location_id=274707&adults=${adults}1&checkin=${ci}&rooms${rooms}1&nights=${nights}`, {
         "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-        "x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa"
-}));
+        "x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa",
+    });
     data.then((value) => {
+        //name and price of hotel 1
         var fhotelName = value.data[0].name;
         var fhotelPrice = value.data[0].price;
-        var shotelName =value.data[1].name;
+        //name and price of hotel 2
+        var shotelName = value.data[1].name;
         var shotelPrice = value.data[1].price;
-        var thotelName= value.data[2].name;
-        var thotelPrice= value.data[2].price;
+        //name and price of hotel 3
+        var thotelName = value.data[2].name;
+        var thotelPrice = value.data[2].price;
+        //use API data to fill in results area for all 3 hotels
         document.getElementById("fhotel").append(fhotelName + " costing around " + fhotelPrice);
         document.getElementById("shotel").append(shotelName + " costing around " + shotelPrice);
         document.getElementById("thotel").append(thotelName + " costing around " + thotelPrice);
-    })
+    });
 });
 
-$("#fly").on("click",function(event) {
+//flights API
+$("#fly").on("click", function (event) {
+    //preventDefault to stop refresh
     event.preventDefault();
+    //departure and return dates and chosen airport
     var dep = $("#dep").val();
     var rtn = $("#rtn").val();
     var airport = $("#airport").val();
 
-    data = (makeAPICall(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en/${airport}-sky/PRG-sky/${dep}?inboundpartialdate=${rtn}`, {
+    data = makeAPICall(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en/${airport}-sky/PRG-sky/${dep}?inboundpartialdate=${rtn}`, {
         "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        "x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa"
-    }));
-        data.then((value)=> {
-            var flight = value.Quotes[0].MinPrice;
-            var carrier = value.Carriers[0].Name;
-            document.getElementById("flightResults").append("The cheapest flight found cost £"+flight + ", flying with "+carrier)
-        })
+        "x-rapidapi-key": "5016226057msh752c3a66045bae2p13d849jsnd78e30e17efa",
+    });
+    data.then((value) => {
+        //use API data to get price and carrier name
+        var flight = value.Quotes[0].MinPrice;
+        var carrier = value.Carriers[0].Name;
+        //use variables to fill out results section
+        document.getElementById("flightResults").append("The cheapest flight found cost £" + flight + ", flying with " + carrier);
+    });
 });
